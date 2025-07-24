5ec18f7fb89350174b8910a137274af09496ec07
kkatalyst <byeongchae44@gmail.com>
2025-07-24 16:30:35 +0900

    간만에 코딩테스트 문제 풀이 + 테스트 코드 세팅

---
+++ src/programmers/level0/중복된_숫자_개수.java
@@ -0,0 +1,23 @@
+package programmers.level0;
+
+/**
+ * 문제 설명 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+ * <p>
+ * 제한사항 1 ≤ array의 길이 ≤ 100 0 ≤ array의 원소 ≤ 1,000 0 ≤ n ≤ 1,000
+ */
+
+public class 중복된_숫자_개수 {
+
+  public static int solution(int[] arr, int n) {
+    int answer = 0;
+
+    for (int number : arr) {
+      if (number == n) {
+        answer++;
+      }
+    }
+
+    return answer;
+  }
+
+}
---
+++ src/test/programmers/level0/중복된_숫자_개수_테스트.java
@@ -0,0 +1,31 @@
+package programmers.level0;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class 중복된_숫자_개수_테스트 {
+
+  /**
+   * [1, 1, 2, 3, 4, 5]	1	2 [0, 2, 3, 4]	1	0
+   */
+
+  @Test
+  public void firstExample() {
+    int[] arr = {1, 1, 2, 3, 4, 5};
+    int n = 1;
+    int answer = 2;
+
+    assertEquals(answer, 중복된_숫자_개수.solution(arr, n));
+  }
+
+  @Test
+  public void secondExample() {
+    int[] arr = {0, 2, 3, 4};
+    int n = 1;
+    int answer = 0;
+
+    assertEquals(answer, 중복된_숫자_개수.solution(arr, n));
+  }
+}
