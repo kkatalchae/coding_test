8ad062013a55f35cbb0a163a162ad1d7e0cb4fa1
kkatalyst <byeongchae44@gmail.com>
2026-01-02 11:03:49 +0900

    백준: 10171 고양이

---
+++ src/Beakjoon/IO_Arithmetic/고양이.java
@@ -0,0 +1,10 @@
+package Beakjoon.IO_Arithmetic;
+
+public class 고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
