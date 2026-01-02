3f4692e52617a11d7cfd3a5bc9b111caede36759
kkatalyst <byeongchae44@gmail.com>
2026-01-02 11:05:00 +0900

    백준: 10172 개

---
+++ src/Beakjoon/IO_Arithmetic/개.java
@@ -0,0 +1,11 @@
+package Beakjoon.IO_Arithmetic;
+
+public class 개 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
