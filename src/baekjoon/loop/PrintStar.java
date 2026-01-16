1f42d43471899f41d1c1bba1fe44664481555696
kkatalyst <byeongchae44@gmail.com>
2026-01-16 17:18:11 +0900

    feat: 백준 2438 풀이

---
+++ src/baekjoon/loop/PrintStar.java
@@ -0,0 +1,19 @@
+package baekjoon.loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrintStar {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int n =  Integer.parseInt(reader.readLine());
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			builder.append("*".repeat(i+1)).append("\n");
+		}
+
+		reader.close();
+		System.out.println(builder);
+	}
+}
